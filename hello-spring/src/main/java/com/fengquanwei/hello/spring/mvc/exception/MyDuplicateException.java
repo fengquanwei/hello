be788ba362a2b1402dd551e5e6ffe2ad4c876efd
fengquanwei <fengquanwei1996@163.com>
2018-06-06 10:29:36 +0800

    使用异常处理器来处理异常

---
+++ hello-spring/src/main/java/com/fengquanwei/hello/spring/mvc/exception/MyDuplicateException.java
@@ -0,0 +1,10 @@
+package com.fengquanwei.hello.spring.mvc.exception;
+
+/**
+ * MyDuplicateException
+ *
+ * @author fengquanwei
+ * @create 2018/6/6 10:21
+ **/
+public class MyDuplicateException extends RuntimeException {
+}
